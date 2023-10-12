1d6456168cf40b8da14fdf89c7aa322bf622293f
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-10-12 23:35:22 +0200

    navbar + chatrooms#show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @chatrooms = Chatroom.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Chatroom.create(name: "Chat#1")
+User.create(email: "manue@lewagon.org", nickname: "Manue", password: "123456")
+User.create(email: "karym@lewagon.org", nickname: "Karym", password: "123456")
