f621357be03e2879820875621162d8df8315ec90
emmanuelle giffard <emmanuelle.giffard@gmail.com>
2023-10-13 03:55:54 +0200

    action cable setup

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+import consumer from "../channels/consumer";
+
+// Connects to data-controller="chatroom-subscription"
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
+  connect() {
+    this.channel = consumer.subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      {received: data => console.log(data)}
+    )
+
+    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+}
